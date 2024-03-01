6cc4d15cc4ade0e607d24b18e2b9d99633a0f54e
SS-ekow <120583131+SS-ekow@users.noreply.github.com>
2024-03-01 22:35:18 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'; 
+import {faPlus, faCircleCheck, faPen, faTrashCan} from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
+
+
 function App() {
+
+  //Tasks State
+  const [toDo, setToDo] = useState([
+    {id: 1, task: "Task 1", completed: false},
+    {id: 2, task: "Task 2", completed: false}
+    
+  ]); 
+
+  // Temp State
+  const [newTask, setnewTask] = useState("");
+  const [updateData, setUpdateData] = useState("");
+
+// Add Task
+const addTask = () => {};
+
+
+
+// Delete Task
+const deleteTask = (id) => {};  
+
+
+// mark as completed
+const markDone = (id) => {};
+
+//cancel update 
+const cancelUpdate = () => {};
+
+// change task 
+const changeTask = (e) => {};
+
+// update task
+const updateTask = () => {};
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+    <br/><br/>
+    <h2>To Do List App</h2>
+    <br/><br/>
+
+    {/* display tasks */}
+
+    
+
+
+      
     </div>
   );
 }
