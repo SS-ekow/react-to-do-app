30de17f1f1ba719e42f73f03fad387fce54dce4d
SS-ekow <120583131+SS-ekow@users.noreply.github.com>
2024-03-02 18:08:40 +0000

    added logic to add tasks

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
 
   //Tasks State
   const [toDo, setToDo] = useState([
-    {id: 1, task: "Task 1", completed: false},
-    {id: 2, task: "Task 2", completed: false}
+    {id: 1, title: "Task 1", completed: false},
+    {id: 2, title: "Task 2", completed: false}
     
   ]); 
 
@@ -21,7 +21,16 @@ function App() {
   const [updateData, setUpdateData] = useState("");
 
 // Add Task
-const addTask = () => {};
+const addTask = () => {
+  if(newTask) {
+    let num = toDo.length + 1;
+    let newEntry = {id: num, title: newTask, completed: false}
+
+    setToDo([...toDo, newEntry]);
+    setnewTask('');
+
+  }
+};
 
 
 
@@ -49,8 +58,82 @@ const updateTask = () => {};
     <br/><br/>
     <h2>To Do List App</h2>
     <br/><br/>
+    
+    {/* update task */}
+    <div className="row">
+      <div className="col">
+        <input type="text" className="form-control form-control-lg" />
+
+      </div>
+      <div className="col-auto">
+        <button className="btn btn-success">
+          <FontAwesomeIcon icon={faPen} />
+          Update 
+        </button>
+        <button className="btn btn-danger" onClick={cancelUpdate}>
+          Cancel
+        </button>
+      </div>
+    </div>
+    <br />
+
+
+
+    {/* Add task */}
+    <div className="row">
+      <div className="col">
+        <input 
+        value = {newTask}
+        onChange={(e)=>setnewTask(e.target.value)}
+        type="text" className="form-control form-control-lg" />
+      </div>
+      <div className="col-auto">
+        <button 
+        onClick ={addTask}
+        className="btn btn-lg btn-success">
+          <FontAwesomeIcon icon={faPlus} />
+          Add Task
+
+        </button>
+      </div>
+    </div>
+    <br />
 
     {/* display tasks */}
+
+    {toDo && toDo.length ? '': 'No tasks...'}
+
+    {toDo && toDo.map((task, index)=>{
+      return (
+        <React.Fragment key = {task.id}>
+          <div className = "col taskBg">
+            <div className={task.completed ? 'done': ''}>
+              <span className='taskNumber'>{index + 1}</span>
+              <span className='taskText'>{task.title}</span>
+
+            </div>
+            <div className="iconsWrap">
+              <span title='completed/ not completed'>
+                <FontAwesomeIcon icon={faCircleCheck} onClick={()=>markDone(task.id)} />
+              </span>
+              <span title="edit">
+                <FontAwesomeIcon icon={faPen} onClick={()=>changeTask(task.id)}/>
+              </span>
+              <span title='Delete'>
+                <FontAwesomeIcon icon={faTrashCan} onClick={()=>deleteTask(task.id)}/>
+              </span>
+
+            </div>
+          </div>
+        
+        </React.Fragment>
+      )
+    })
+
+    }
+
+
+
 
     
 
